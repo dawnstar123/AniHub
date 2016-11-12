b8f5be15d3216c4fb32514c98d7d7b9eb8aeec91
usabhinav <usabhinav@gmail.com>
2016-11-11 20:04:55 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function start(animalName){
+    $(document).ready(function(){
+        $('body').append("<div class='card'>\
+  <div class='container'>\
+    <h4><b>" + animalName + "</b></h4>\
+    <p>Interior Designer</p>\
+  </div>\
+</div>");
+    })
+}
+start("Your mother");
