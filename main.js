ea600f70358dd066831fa52813a43d2818589925
usabhinav <usabhinav@gmail.com>
2016-11-11 22:07:59 -0800

    Finished function to make cards

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function start(animalName){
     $(document).ready(function(){
         $('body').append("<div class='card'>\
-  <div class='container'>\
-    <h4><b>" + animalName + "</b></h4>\
-    <p>Interior Designer</p>\
-  </div>\
+        <img src='img_avatar2.png' alt='Avatar' style='width:100%'>\
+<div class='container'>\
+<h4><b>" + animalName + "</b></h4>\
+<p>Interior Designer</p>\
+</div>\
 </div>");
     })
 }
 start("Your mother");
-
-// Your mother!
+start("Your father");
+start("your mother");
