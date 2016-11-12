fbb68925f4b023297b8985b703747309424763bb
usabhinav <usabhinav@gmail.com>
2016-11-12 00:22:44 -0800

    Added break after each card

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function createCard(animalName){
     $(document).ready(function(){
-        $('body').append("<div class='card'>\
-        <img src='img_avatar2.png' alt='Avatar' style='width:100%'>\
+	$('body').append("<br></br>");
+        $('body').append("<div class='card' style='margin-bottom: 100px'>\
+        <img src='http://www.w3schools.com/bootstrap/img_avatar4.png' alt='Avatar' style='width:100%'>\
 <div class='container'>\
 <h4><b>" + animalName + "</b></h4>\
 <p>Interior Designer</p>\
 </div>\
 </div>");
+	
     })
 }
 createCard("Your mother");
