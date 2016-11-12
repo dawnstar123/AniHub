c4f353bcecdc2973baee6f90b35626daeb42a120
usabhinav <usabhinav@gmail.com>
2016-11-12 02:01:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function createCard(animalName){
     $(document).ready(function(){
-	$('body').append("<br></br>");
-        $('body').append("<div class='card' style='margin-bottom: 100px'>\
+        $('body').append("<div class='card'>\
         <img src='http://www.w3schools.com/bootstrap/img_avatar4.png' alt='Avatar' style='width:100%'>\
 <div class='container'>\
 <h4><b>" + animalName + "</b></h4>\
 <p>Interior Designer</p>\
 </div>\
 </div>");
-	
+	$('body').append("<br></br>");
     })
 }
 createCard("Your mother");
